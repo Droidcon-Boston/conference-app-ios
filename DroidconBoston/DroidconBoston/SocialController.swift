160b749b505c43c5b4cf6e8197ce101adce70f3c
Justin Poliachik <jpoliachik@gmail.com>
2017-03-16 21:19:28 -0400

    Barebones project setup with TabBarController

---
+++ DroidconBoston/DroidconBoston/SocialController.swift
@@ -0,0 +1,9 @@
+//
+//  SocialController.swift
+//  DroidconBoston
+//
+//  Created by Justin Poliachik on 3/16/17.
+//  Copyright © 2017 Droidcon Boston. All rights reserved.
+//
+
+import Foundation
