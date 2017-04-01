669aeca4f76d2a7601123cfbacc967d93dfcd24a
Justin Poliachik <jpoliachik@gmail.com>
2017-04-01 16:40:59 -0400

    Agenda tableview and custom cell

--- DroidconBoston/DroidconBoston/AgendaController.swift
+++ DroidconBoston/DroidconBoston/AgendaController.swift
@@ -7,17 +7,68 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class AgendaController: UIViewController {
+class AgendaController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var events: [Event] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 100
+        tableView.separatorColor = UIColor.themeGreenAccent
+        
+        AgendaAPI.getAgendaLocal { (events) in
+            
+            if let events = events {
+                self.events = events;
+                self.tableView.reloadData()
+            } else {
+                // handle error
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AgendaCell", for: indexPath) as! AgendaCell
+        
+        // clear out image first, to prevent a recycling flash
+        cell.userImage.image = nil
+        
+        let eventObject = events[indexPath.row]
+        
+        cell.name.text = eventObject.name
+        cell.talkTitle.text = eventObject.talk
+        
+        if let imageUrl = URL(string: eventObject.photoUrl) {
+            cell.userImage?.af_setImage(withURL: imageUrl)
+        } else {
+            cell.userImage.image = nil
+        }
+        
+        
+        return cell
+    }
+
     
     
 }
