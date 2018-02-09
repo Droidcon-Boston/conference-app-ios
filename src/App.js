457d1c8fe38400f108a4535303da33eb17d5811b
Justin Poliachik <justin.poliachik@apiture.com>
2018-02-08 20:45:39 -0500

    launch image

--- src/App.js
+++ src/App.js
@@ -2,35 +2,30 @@ import { Navigation } from "react-native-navigation";
 
 import { registerScreens } from "./screens";
 
-registerScreens(); // this is where you register all of your app's screens
+registerScreens();
 
 Navigation.startSingleScreenApp({
   screen: {
     title: "Agenda",
-    screen: "AgendaContainer", // unique ID registered with Navigation.registerScreen
-    navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
-    navigatorButtons: {}, // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
+    screen: "AgendaContainer",
+    navigatorStyle: {},
+    navigatorButtons: {},
   },
   drawer: {
-    // optional, add this if you want a side menu drawer in your app
     left: {
-      // optional, define if you want a drawer from the left
-      screen: "DrawerContainer", // unique ID registered with Navigation.registerScreen
-      passProps: {}, // simple serializable object that will pass as props to all top screens (optional)
-      disableOpenGesture: false, // can the drawer be opened with a swipe instead of button (optional, Android only)
-      // fixedWidth: 500, // a fixed width you want your left drawer to have (optional)
+      screen: "DrawerContainer",
+      passProps: {},
+      disableOpenGesture: false,
     },
     style: {
-      // ( iOS only )
-      drawerShadow: true, // optional, add this if you want a side menu drawer shadow
-      contentOverlayColor: "rgba(0,0,0,0.25)", // optional, add this if you want a overlay color when drawer is open
-      leftDrawerWidth: 75, // optional, add this if you want a define left drawer width (50=percent)
+      drawerShadow: true,
+      contentOverlayColor: "rgba(0,0,0,0.25)",
+      leftDrawerWidth: 75,
     },
-    type: "MMDrawer", // optional, iOS only, types: 'TheSideBar', 'MMDrawer' default: 'MMDrawer'
-    animationType: "slide", //optional, iOS only, for MMDrawer: 'door', 'parallax', 'slide', 'slide-and-scale'
-    // for TheSideBar: 'airbnb', 'facebook', 'luvocracy','wunder-list'
-    disableOpenGesture: false, // optional, can the drawer, both right and left, be opened with a swipe instead of button
+    type: "MMDrawer",
+    animationType: "slide",
+    disableOpenGesture: false,
   },
-  passProps: {}, // simple serializable object that will pass as props to all top screens (optional)
-  animationType: "slide-down", // optional, add transition animation to root change: 'none', 'slide-down', 'fade'
+  passProps: {},
+  animationType: "slide-down",
 });
