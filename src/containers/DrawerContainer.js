fbbbb537f314262bba3c598df0f6d01926efc33d
Justin Poliachik <justin.poliachik@apiture.com>
2018-02-10 15:15:24 -0500

    add logo image to drawer

--- src/containers/DrawerContainer.js
+++ src/containers/DrawerContainer.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Dimensions, Text, Image } from "react-native";
 import { connect } from "react-redux";
+
+const logo = require("../../assets/logo_large.png");
+
+import Constants from "../util/Constants";
 
 function mapStateToProps(state) {
   return {};
 }
 class DrawerContainer extends Component {
-  static title = "Drawer";
   render() {
-    return <View style={{ backgroundColor: Colors.white, flex: 1 }} />;
+    const { width } = Dimensions.get("window");
+    return (
+      <View style={{ backgroundColor: Colors.white, flex: 1 }}>
+        <View style={{ backgroundColor: Colors.black, justifyContent: "center", alignItems: "center" }}>
+          <Image
+            source={logo}
+            resizeMode="contain"
+            style={{ width: width * (Constants.drawerWidthPercent / 100) * 0.7, height: 150 }}
+          />
+        </View>
+      </View>
+    );
   }
 }
 export default connect(mapStateToProps)(DrawerContainer);
