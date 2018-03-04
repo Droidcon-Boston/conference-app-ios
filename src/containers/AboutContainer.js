ddbad6749c80b823607a46e6e3f1c875e513e3a1
Justin Poliachik <jpoliachik@gmail.com>
2018-03-04 09:38:08 -0500

    About UI and content

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Image, Dimensions, ScrollView } from "react-native";
 import { connect } from "react-redux";
+import { Text } from "../components";
 import { setRootNavigatorActions } from "../util/UtilNavigation";
+import Colors from "../util/Colors";
+
+const logo = require("../../assets/logo_large.png");
 
 function mapStateToProps(state) {
-  return {};
+  return {
+    about: state.conf.get("about"),
+  };
 }
 class AboutContainer extends Component {
   constructor(props) {
@@ -17,7 +23,18 @@ class AboutContainer extends Component {
     });
   }
   render() {
-    return <View style={{ flex: 1 }} />;
+    const { width } = Dimensions.get("window");
+
+    return (
+      <ScrollView style={{ flex: 1, backgroundColor: Colors.black }}>
+        <View style={{ paddingHorizontal: 20, justifyContent: "center", alignItems: "center" }}>
+          <Image source={logo} resizeMode="contain" style={{ width: width * 0.7, height: 120 }} />
+        </View>
+        <View style={{ padding: 20 }}>
+          <Text white>{this.props.about}</Text>
+        </View>
+      </ScrollView>
+    );
   }
 }
 export default connect(mapStateToProps)(AboutContainer);
