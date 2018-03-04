661f31b352fb2c2994ce6dfa33eaf2721e594568
Justin Poliachik <jpoliachik@gmail.com>
2018-03-04 10:41:47 -0500

    Social section

--- src/containers/SocialContainer.js
+++ src/containers/SocialContainer.js
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, TouchableOpacity, FlatList, Linking } from "react-native";
 import { connect } from "react-redux";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { Text } from "../components";
 import { setRootNavigatorActions } from "../util/UtilNavigation";
+import Colors from "../util/Colors";
+
+const SOCIAL_DATA = [
+  {
+    key: "facebook",
+    name: "Facebook",
+    url: "https://www.facebook.com/droidconbos",
+    icon: "facebook",
+  },
+  {
+    key: "twitter",
+    name: "Twitter",
+    url: "https://twitter.com/droidconbos",
+    icon: "twitter",
+  },
+  {
+    key: "instagram",
+    name: "Instagram",
+    url: "https://www.instagram.com/droidconbos/",
+    icon: "instagram",
+  },
+  {
+    key: "linkedin",
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/groups/8586436/profile",
+    icon: "linkedin",
+  },
+  {
+    key: "youtube",
+    name: "YouTube",
+    url: "https://www.youtube.com/channel/UClv2UAFbLxopI8_9fscZ-ew",
+    icon: "youtube-play",
+  },
+];
+
+const Separator = () => {
+  return <View style={{ height: 1, backgroundColor: Colors.grey300 }} />;
+};
 
 function mapStateToProps(state) {
   return {};
@@ -16,8 +57,52 @@ class SocialContainer extends Component {
       title: "Social",
     });
   }
+
+  onSelect(item) {
+    Linking.canOpenURL(item.url)
+      .then(supported => {
+        if (!supported) {
+          console.log("Can't handle url: " + item.url);
+        } else {
+          return Linking.openURL(item.url);
+        }
+      })
+      .catch(err => console.error("An error occurred", err));
+  }
+
+  renderCell(item) {
+    return (
+      <TouchableOpacity
+        onPress={() => this.onSelect(item)}
+        style={{ flexDirection: "row", height: 45, alignItems: "center" }}
+      >
+        <View
+          style={{
+            backgroundColor: Colors.green,
+            height: 30,
+            width: 30,
+            borderRadius: 15,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Icon name={item.icon} size={18} color={Colors.black} />
+        </View>
+        <Text grey800 style={{ marginHorizontal: 12 }}>
+          {item.name}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
   render() {
-    return <View style={{ flex: 1 }} />;
+    return (
+      <FlatList
+        style={{ padding: 12 }}
+        data={SOCIAL_DATA}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item }) => this.renderCell(item)}
+      />
+    );
   }
 }
 export default connect(mapStateToProps)(SocialContainer);
