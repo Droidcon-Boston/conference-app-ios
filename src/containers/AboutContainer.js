0930a5f39a8fb7224d297b065cded2d02dbb58a9
Justin Poliachik <jpoliachik@gmail.com>
2018-03-12 20:34:36 -0400

    Update AboutContainer

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { View, StyleSheet, Image, Dimensions, ScrollView } from "react-native";
 import { connect } from "react-redux";
+import striptags from "striptags";
 import { Text } from "../components";
 import { setRootNavigatorActions } from "../util/UtilNavigation";
 import Colors from "../util/Colors";
 
 const logo = require("../../assets/logo_large.png");
+const background_asteroids = require("../../assets/background_asteroids.png");
 
 function mapStateToProps(state) {
   return {
@@ -24,14 +26,28 @@ class AboutContainer extends Component {
   }
   render() {
     const { width } = Dimensions.get("window");
-
+    const textContent = striptags(this.props.about);
     return (
-      <ScrollView style={{ flex: 1, backgroundColor: Colors.black }}>
-        <View style={{ paddingHorizontal: 20, justifyContent: "center", alignItems: "center" }}>
-          <Image source={logo} resizeMode="contain" style={{ width: width * 0.7, height: 120 }} />
+      <ScrollView style={{ flex: 1, backgroundColor: Colors.backgroundColor }}>
+        <View style={{ backgroundColor: Colors.black }}>
+          <Image style={{ width: width, height: 160, opacity: 0.4 }} source={background_asteroids} />
+          <View
+            style={{
+              paddingHorizontal: 20,
+              justifyContent: "center",
+              alignItems: "center",
+              position: "absolute",
+              top: 0,
+              right: 0,
+              bottom: 0,
+              left: 0,
+            }}
+          >
+            <Image source={logo} resizeMode="contain" style={{ width: width * 0.7, height: 120 }} />
+          </View>
         </View>
         <View style={{ padding: 20 }}>
-          <Text white>{this.props.about}</Text>
+          <Text>{textContent}</Text>
         </View>
       </ScrollView>
     );
