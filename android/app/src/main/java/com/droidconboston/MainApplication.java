dd657e859906e82efeb853d1d70c5af90774aaa3
Justin Poliachik <jpoliachik@gmail.com>
2018-03-17 22:11:25 -0400

    use react-native-fast-image for better image caching

--- android/app/src/main/java/com/droidconboston/MainApplication.java
+++ android/app/src/main/java/com/droidconboston/MainApplication.java
@@ -3,6 +3,8 @@ package com.droidconboston;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.reactnativenavigation.NavigationReactPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+            new FastImageViewPackage(),
             new VectorIconsPackage(),
             new RNFirebasePackage(),
             new NavigationReactPackage()
--- src/components/SpeakerImage.js
+++ src/components/SpeakerImage.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from "react";
 import { View, Image } from "react-native";
 import PropTypes from "prop-types";
+import FastImage from "react-native-fast-image";
 
 import Colors from "../util/Colors";
 import Style from "../util/Style";
@@ -14,7 +15,7 @@ export default class SpeakerImage extends PureComponent {
     return (
       <Image
         {...this.props}
-        source={{ uri: this.props.url, cache: "force-cache" }}
+        source={{ uri: this.props.url, priority: FastImage.priority.high }}
         style={[{ backgroundColor: Colors.grey200 }, this.props.style]}
       />
     );
